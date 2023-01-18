2b7e2071c19bcd1adfa190ab46a58d57ddfe0952
XCODEJET <102839813+prabhasha2006@users.noreply.github.com>
2023-01-18 19:29:45 +0530

    ```Kumuthu Prabhasha``` XCODEJET

---
+++ controll/conversation-header-fix.js
@@ -0,0 +1,15 @@
+// * on scroll fixed header
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById('CONVERSATION_HEAD_FIX')
+var sticky = header.offsetTop;
+header.classList.add("sticky");
+
+function myFunction() {
+
+}
+
+//* active type bar
+document.getElementById('MESSAGE_BOX').focus()
+
